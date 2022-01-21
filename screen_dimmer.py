22dc3f75d3d7c75cde5a3af29daf48c4d3abe280
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-01-20 23:52:08 -0500

    Fixed bug with sbc library

--- screen_dimmer.py
+++ screen_dimmer.py
@@ -6,10 +6,12 @@ def get_displays() -> int:
     try:
         # No one has more than 5 monitors
         for i in range(5):
-            sbc.set_brightness("+1", i)
+            sbc.set_brightness(20, i)
             number_of_displays += 1
+            print(number_of_displays)
     except (LookupError, sbc.ScreenBrightnessError):
         return number_of_displays
+    return number_of_displays
 
 
 def change_all_brightness(brightness: int):
